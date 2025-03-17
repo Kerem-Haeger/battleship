de8692b167d4f6a171633cf8647fdfa36f959e99
Kerem-Haeger <mvtf3siy@students.codeinstitute.net>
2025-03-17 06:59:11 +0000

    Work on layout of grid

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 const createBoard = (boardId) => {
     const board = document.getElementById(boardId);
-    const letters = "ABCDEFGHIJ";
+    const letters = "abcdefghij";
 
     for (let row = 0; row < 10; row++) {
         for (let col = 1; col <= 10; col++) {
             const cell = document.createElement("div");
-            cell.classList.add("cell");
-            cell.id = `${letters[row]}${col}`; // e.g., A1, B2
+            cell.classList.add("cell"); // Add the class cell so can be styled by CSS
+            cell.id = `${letters[row]}${col}`; // e.g., a1, b2
             cell.addEventListener("click", (e) => handleCellClick(e.target));
             board.appendChild(cell);
         }
