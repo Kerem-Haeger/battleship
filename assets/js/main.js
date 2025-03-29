3d79ca5ee7201b41cecc9217f9f0fa2540fe7cd6
Kerem-Haeger <mvtf3siy@students.codeinstitute.net>
2025-03-29 18:02:46 +0000

    Call modal on page load, only call game starting functions when user closes modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,15 +12,24 @@ import {
     highlightUserCells
 } from './events.js'
 
+/**
+ * On loading the user is presented with an obligatory modal to enter their name
+ */
+document.addEventListener("DOMContentLoaded", function () {
+    let instructionModal = new bootstrap.Modal(document.getElementById("static-backdrop"));
+    instructionModal.show();
 
+    document.getElementById("got-it").addEventListener("click", (e) => {
+        // These will be called when the player presses "Start" in the startup/instruction modal
+        createBoard("player-board");
+        createBoard("computer-board");
+        computerPlaceShips();
+        // This should be removed for the game, it is only for debugging!
+        colorShipCells("computer-board");
+        instructionModal.hide();
+    });
+});
 
-// These will be called when the player presses "Start" in the startup/instruction modal
-createBoard("player-board");
-createBoard("computer-board");
-
-computerPlaceShips();
-// This should be removed for the game, it is only for debugging!
-colorShipCells("computer-board");
 
 highlightUserCells();
 userPlaceShips();
